8489c9ddbb19802a3ae20187a0c31eb4c6425b00
如梦技术 <596392912@qq.com>
2019-02-23 00:08:13 +0800

    :tada: Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# mica 演示项目
+
+## 链接
+mica 源码：https://github.com/lets-mica
+
+mica 文档：https://www.dreamlu.net/#/doc/installation
---
+++ src/test/java/net/dreamlu/MicaExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package net.dreamlu;
+
+import net.dreamlu.mica.test.MicaBaseTest;
+import net.dreamlu.mica.test.MicaBootTest;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@MicaBootTest("mica-example")
+public class MicaExampleApplicationTests extends MicaBaseTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
